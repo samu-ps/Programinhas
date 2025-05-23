31cbfc3b540e8a471b06fb62f85a53e8f7349081
samu-ps <samuelhenry303@gmail.com>
2025-05-23 19:16:52 -0300

    funcionando

---
+++ Cadastro/conectar.php
@@ -0,0 +1,28 @@
+<!-- Conexão com o banco de dados -->
+
+<?php
+    //Informar os dados do servidor DB
+    // Criar variável de memória para conter as informações de acesso ao DB
+    $servidor = 'localhost';
+    $banco = 'vendas';
+    $usuario = 'root';
+    $senha = '';
+    
+    // Executar procedimento de tentativa e erro
+    try { //tentar conectar ao DB:
+        //criar variável do tipo DPO
+        $pdo = new PDO("mysql:dbname=$banco; hostname=$servidor; charset=utf8","$usuario","$senha");
+        echo '<strong>Conectado com sucesso</strong>';
+        echo '<br>';
+    } catch (Exception $e) {//se der errado:
+        // Criamos a variável do tipo Exception(e) para receber os erros de conexão retornados pelo DB
+        echo '<strong class="erro">Erro ao conectar no banco de dados.</strong>';
+        echo '<br>';
+        echo '<strong class="erro">Entre em contato com os alunos da Informática da ETMSL.</strong>';
+        echo '<br>';
+        // Exibe a mensagem de erro 
+        echo $e;
+    }
+    
+    
+?>
--- Cadastro/salvando.php
+++ Cadastro/salvando.php
@@ -1,23 +1,25 @@
 <!-- Recebe o conteúdo dos inputs e armazena em variáveis locais para serem salvas(insert) no DB -->
 <?php
 //realizar conexão com o DB
-    require_once("./conexao.php");
+    require_once("./conectar.php");
 //receber as variáveis dos INPUTs E (arquivo index.php)
 //armazenar em variáveis locais (criadas no arquivo salvar.php)
 
 //criar uma sessão para variáveis globais
     @session_start();
     //criar variáveis locais para armazenar o conteúdo das variáveis globais (variáveis dos inputs)
-    $user = $_POST['usuario']; //armazena na variável local o conteúdo do input usuario
+    $user = $_POST['nome']; //armazena na variável local o conteúdo do input usuario
+    $cpf = $_POST['cpf'];
     $fone = $_POST['telefone'];
     $passwd = $_POST['senha'];
 
     //tentar enviar(insert) as informações para o DB
     try {
-        $query = "INSERT INTO estoque.usuarios (nomeUsuario, foneUsuario, senhaUsuario)
-        VALUES (:user, :fone, :passwd)";
+        $query = "INSERT INTO vendas.clientes (nomeCli, cpfCli, foneCli, senhaCli)
+        VALUES (:user, :cpf, :fone, :passwd)";
         $stmt=$pdo->prepare($query);
         $stmt->bindParam(':user', $user);
+        $stmt->bindParam(':cpf', $cpf);
         $stmt->bindParam(':fone', $fone);
         $stmt->bindParam(':passwd', $passwd);
 
@@ -30,3 +32,7 @@
         echo $e;
     }   
 ?>
+<!-- nomeCli varchar(200) 
+cpfCli varchar(14) 
+foneCli varchar(14) 
+senhaCli -->
